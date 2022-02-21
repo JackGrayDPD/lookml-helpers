543bbb1f82897a52cd4e27cc032d65302b34549e
JackGrayDPD <jack.gray@dpdgroup.co.uk>
2022-02-21 15:28:40 +0000

    add generateFieldnames, generateTests. Moved logic into generateTests

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # lookml-helpers
+
+## looker-generate-tests
+Copy/paste a view's file contents into looker_view.lookml, then run ```node generateTests```. This will create or overwrite the file looker_tests.lookml with a basic field checking test.
+
+## looker-generate-fieldnames
+Copy/paste a view's file contents into looker_view.lookml, then run ```node generateFieldnames```. This will create or overwrite the file looker_fields.lookml with a comma-seperated list of all fields in the view.
---
+++ generateFieldnames.js
@@ -0,0 +1,42 @@
+/**
+ * looker-generate-fieldnames
+ * This script will take a Looker view file and produce a comma-separated list of all fieldnames for that view in looker_views.lookml.
+ * Save your view contents in looker_view.lkml and run `node generateFieldnames` from this project.
+ * Results will write to looker_fields.lkml. File contents will be overwritten every time the script is run
+ */
+
+const fs = require('fs');
+
+const inputFile = __dirname + '/looker_view.lookml';
+const outputFile = __dirname + '/looker_fields.lookml';
+
+function readError(err, data) {
+	if (err) {
+		console.log(err);
+	} else {
+		return data
+	}
+}
+
+function alphabeticalSort(a, b) {
+	if (a < b) return -1;
+	if (a > b) return 1;
+}
+
+const input = fs.readFileSync(inputFile, "utf8", readError).toString().split("\n");
+var fieldNames = [];
+input.forEach(line => {
+	if (line.trim().startsWith("dimension:") || line.trim().startsWith("measure:")) {
+		var fieldName = line.trim().split(":")[1].split("{")[0].trim();
+		fieldNames.push(fieldName);
+	}
+});
+
+fieldNames = fieldNames.sort(alphabeticalSort)
+const fieldList = fieldNames.join(",");
+
+// fs.writeFileSync(outputFile, fieldList);
+// console.log("Tests written!");
+
+fs.writeFileSync(outputFile, fieldList);
+console.log(`Fieldnames written to ${outputFile}`);
