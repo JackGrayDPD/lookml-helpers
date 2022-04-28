bd83ab4d98ce56b5dd68bc2e7295cb5e29b0df27
JackGrayDPD <jack.gray@dpdgroup.co.uk>
2022-04-28 16:15:29 +0100

    fixed generateGlossary

--- generateGlossary.js
+++ generateGlossary.js
@@ -6,69 +6,53 @@
  */
 
 const fs = require('fs');
+const lookmlParser = require('lookml-parser')
 const { readError } = require('./functions');
 
 const inputFile = __dirname + '/files/input/looker_view.lookml';
 const outputFile = __dirname + '/files/output/looker_glossary.csv';
 const testFile = __dirname + '/files/output/test.txt';
 
-const input = fs.readFileSync(inputFile, "utf8", readError).toString().split(
-	/((dimension|measure): ([A-Za-z_0-9]+) {\s([\s]+[A-Za-z0-9_]*:\s*[A-Za-z0-9_;.${} "@?=]+)+)/g
-);
+// const { view } = lookmlParser.parse(fs.readFileSync(inputFile, "utf8", readError).replace(/(\r\n|\n|\r)/gm, " ").trim());
+const { view } = lookmlParser.parse(fs.readFileSync(inputFile, "utf8", readError));
 
-var fields = [];
-input.forEach(el => {
-	if (el.startsWith("dimension:") || el.startsWith("measure:")) {
-		fields.push(el.split('\n'));
-	}
-})
-fields.forEach((field, idx) => {
-	field.forEach((f, i) => {
-		fields[idx][i] = f.trim();
-	})
-})
-// Each field is now an array in fields[] with one element per line
+const viewName = Object.keys(view)[0];
 
-fields.forEach((field, idx) => {
-	field.forEach((f, i) => { // inside each field[i] = f
-		if (f.startsWith('dimension:') || f.startsWith('measure:')) {
-			var parts = f.split(':'); // parts = ['dimension','fieldname {']
-			if (parts[1].endsWith('{')) {
-				parts[1] = parts[1].slice(0, -1);
-			}
-			fields[idx][i] = { fieldType: parts[0], fieldName: parts[1].trim() }
-		} else {
-			var parts = f.split(':'); // parts = [key,value]
-			fields[idx][i] = { key: parts[0], value: parts[1].replace(';;', '').trim() }
-		}
-	})
-})
-
-var fields2 = []
-fields.forEach((field, idx) => {
-	var newFieldObj = {};
-	field.forEach((f, i) => {
-		if (i === 0) {
-			newFieldObj.fieldType = f.fieldType
-			newFieldObj.fieldName = f.fieldName
-		} else {
-			newFieldObj.params = { ...newFieldObj.params, [f.key]: f.value }
-		}
-	})
-	fields2.push(newFieldObj);
-})
+const dimensions = view[viewName].dimension;
+const dimensionKeys = Object.keys(dimensions);
+const measures = view[viewName].measure;
+const measureKeys = Object.keys(measures);
 
 var csvLines = 'Field Name,Field Type,Data Type,SQL,Label,Group Label,Group Item Label,Description\n';
-fields2.forEach(field => {
-	var fieldName = field.fieldName;
-	var fieldType = field.fieldType;
-	var type = field.params.type || '';
-	var sql = field.params.sql || '';
-	var desc = field.params.description || '';
-	var label = field.params.label || '';
-	var group_label = field.params.group_label || '';;
-	var group_item_label = field.params.group_item_label || '';;
+
+const clean = function (str) {
+	return '"' + str.replace(/( )+/gm, " ").trim().replace(/"/g, '""') + '"';
+}
+dimensionKeys.forEach(key => {
+	var fieldName = clean(dimensions[key]['$name']);
+	var fieldType = clean(dimensions[key]['$type']);
+	var type = clean(dimensions[key].type) || '';
+	var sql = clean(dimensions[key].sql) || '';
+	var desc = clean(dimensions[key].description) || '';
+	var label = clean(dimensions[key].label) || '';
+	var group_label = clean(dimensions[key].group_label) || '';
+	var group_item_label = clean(dimensions[key].group_item_label) || '';
+
 	csvLines = csvLines + [fieldName, fieldType, type, sql, label, group_label, group_item_label, desc].join(',').concat('\n');
-})
+});
+
+measureKeys.forEach(key => {
+	var fieldName = clean(measures[key]['$name']);
+	var fieldType = clean(measures[key]['$type']);
+	var type = clean(measures[key].type) || '';
+	var sql = clean(measures[key].sql) || '';
+	var desc = clean(measures[key].description) || '';
+	var label = clean(measures[key].label) || '';
+	var group_label = clean(measures[key].group_label) || '';
+	var group_item_label = clean(measures[key].group_item_label) || '';
+
+	csvLines = csvLines + [fieldName, fieldType, type, sql, label, group_label, group_item_label, desc].join(',').concat('\n');
+});
+
 fs.writeFileSync(outputFile, csvLines);
 console.log(`Glossary written to ${outputFile}`);
